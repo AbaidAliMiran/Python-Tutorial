67df43eb5f735e44a11f33e9327fff519636b3fe
Abaid Ali Miran <aabaid737@gmail.com>
2023-12-16 18:54:44 +0500

    Basic python program
    This program introduces the user to Python's basic functionalities like printing, taking input, and performing basic arithmetic operations.

---
+++ Basic of Python, Variables and Data types/basic.py
@@ -0,0 +1,17 @@
+print("Welcome to the python basic!")
+name = input("What is your name:")
+print("Hello", name , "! Lets do some basic calculations.")
+num1 = float(input("Enter a number:"))
+num2 = float(input("Enter another number:"))
+
+# performing basic arithmetic operations
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+
+# Displaying the results
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
