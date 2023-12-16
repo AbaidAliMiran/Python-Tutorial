4db2b0e67be27cefbe1d80592178757c2973ca64
Abaid Ali Miran <aabaid737@gmail.com>
2023-12-16 18:56:07 +0500

    Variables and Data types
    This program showcases the usage of variables and different data types in Python.

---
+++ Basic of Python, Variables and Data types/variable_and_data_types.py
@@ -0,0 +1,18 @@
+# Variables and Data types
+name = "Saad" # string variable
+age = 25 # integer variable
+height = 1.75 # float variable
+is_student = True # boolean variable
+
+# Displaying information
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Checking data types
+print("Data Types")
+print("Name is of type:", type(name))
+print("Age is of type:", type(age))
+print("Height is of type:", type(height))
+print("Is Student is of type:", type(is_student))
