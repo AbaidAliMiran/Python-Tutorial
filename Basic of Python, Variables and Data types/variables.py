c601741ce73cdc5e544b88dd3507d9db99b0a58e
Abaid Ali Miran <aabaid737@gmail.com>
2023-12-16 18:55:42 +0500

    Variables
    This program showcases the usage of variables in Python.

---
+++ Basic of Python, Variables and Data types/variables.py
@@ -0,0 +1,11 @@
+# Variables
+name = "Saad" # string variable
+age = 25 # integer variable
+height = 1.75 # float variable
+is_student = True # boolean variable
+
+# Displaying information
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
