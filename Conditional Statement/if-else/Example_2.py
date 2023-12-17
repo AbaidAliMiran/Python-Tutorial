dada92d36c68be1a266d476e00d70443b964affb
Abaid Ali Miran <aabaid737@gmail.com>
2023-12-17 18:13:36 +0500

    Even/Odd
    This  program asks the user for a number and then tells them if it's an even number or an odd number.

---
+++ Conditional Statement/if-else/Example_2.py
@@ -0,0 +1,7 @@
+# How can you check if a number is even or odd using if_else ?
+
+number =int(input("Enter a Number:"))
+if number%2==0:
+    print(number," is even")
+else:
+    print(number," is odd")
