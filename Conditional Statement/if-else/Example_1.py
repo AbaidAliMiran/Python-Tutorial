391eeb0f70c526f44ec2384296399b5116c91a71
Abaid Ali Miran <aabaid737@gmail.com>
2023-12-17 18:10:12 +0500

    Eligibility Checker
    This Python program prompts the user to input their age and determines eligibility to vote based on whether the entered age is above 18 or not.

---
+++ Conditional Statement/if-else/Example_1.py
@@ -0,0 +1,6 @@
+# How can you determine if a person is eligible to vote based on their age using if-else?
+age = int(input("Enter Your Age:"))
+if age>18:
+    print("You Are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
