a4a30dbbb0decd650cff183d13a892429415c1ab
Abaid Ali Miran <aabaid737@gmail.com>
2023-12-23 20:29:18 +0500

    Check Vowel
    This program takes a character input and determines if it's a vowel by comparing it against a set of vowels using if-else statements. If the input matches any vowel (both uppercase and lowercase), it prints that it's a vowel; otherwise, it indicates that it's not a vowel.

---
+++ Conditional Statement/if-else/Example_3.py
@@ -0,0 +1,6 @@
+# How do you write a program that checks if a given character is vowel or not using if _else?
+ch = input("Enter any character:")
+if ch == 'a' or ch == 'A' or ch == 'e' or ch == 'E' or ch == 'o' or ch == 'O' or ch == 'i' or ch == 'I' or ch == 'u' or ch == 'U':
+    print(ch, " is vowel.")
+else:
+    print(ch, " is not vowel.")
