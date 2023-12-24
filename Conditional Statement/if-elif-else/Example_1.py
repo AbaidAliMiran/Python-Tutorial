4f8d87cea84fa7c6f58196cb1acca17b8bb79d75
Abaid Ali Miran <aabaid737@gmail.com>
2023-12-24 19:21:09 +0500

    Number Sign Checker
    This program determines if a user-input number is positive, negative, or zero using an if-elif-else structure. It categorizes the entered number as zero, positive, or negative, offering a clear indication of its sign.

---
+++ Conditional Statement/if-elif-else/Example_1.py
@@ -0,0 +1,8 @@
+# How can you determine if a number is positive, negative or zero using if-else-if?
+number = int(input("Enter a number:"))
+if number == 0:
+    print("The entered number is zero.")
+elif number > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")
